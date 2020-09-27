74a12a085fc44647db9afb5726fbe0ab6892c1a1
Ryzee119 <wendland@live.com.au>
2020-09-27 16:37:39 +0930

    ci: correctly creates hex from elf

--- .github/workflows/ci_build.sh
+++ .github/workflows/ci_build.sh
@@ -10,6 +10,8 @@ mkdir build
 cd build
 cmake ..
 make VERBOSE=1
-
+avr-objcopy -j .text -j .data -O ihex ogx360_32u4_master.elf ogx360_32u4_master.hex
+avr-objcopy -j .text -j .data -O ihex ogx360_32u4_master_steelbattalion.elf ogx360_32u4_master_steelbattalion.hex
+avr-objcopy -j .text -j .data -O ihex ogx360_32u4_slave.elf ogx360_32u4_slave.hex
 # List files to confirm it worked
 ls -la *
