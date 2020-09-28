d531e3a6e0139080143fd2c7e38cefedabf8bbbb
Ryzee119 <wendland@live.com.au>
2020-09-28 18:26:18 +0930

    Update XBOXONE.h

--- Firmware/ogx360_32u4/lib/USB Host Shield/XBOXONE.h
+++ Firmware/ogx360_32u4/lib/USB Host Shield/XBOXONE.h
@@ -126,11 +126,30 @@ public:
          * @return     Returns true if the device's VID and PID matches this driver.
          */
         virtual bool VIDPIDOK(uint16_t vid, uint16_t pid) {
-                return ((vid == XBOX_VID1 || vid == XBOX_VID2 || vid == XBOX_VID3 || vid == XBOX_VID4 || vid == XBOX_VID5 || vid == XBOX_VID6  || vid == XBOX_VID7) &&
-                    (pid == XBOX_ONE_PID1 || pid == XBOX_ONE_PID2 || pid == XBOX_ONE_PID3 || pid == XBOX_ONE_PID4 ||
-                        pid == XBOX_ONE_PID5 || pid == XBOX_ONE_PID6 || pid == XBOX_ONE_PID7 || pid == XBOX_ONE_PID8 ||
-                        pid == XBOX_ONE_PID9 || pid == XBOX_ONE_PID10 || pid == XBOX_ONE_PID11 || pid == XBOX_ONE_PID12 ||
-						pid == XBOX_ONE_PID13 || pid == XBOX_ONE_PID14 || pid == XBOX_ONE_PID15));
+                return ((vid == XBOX_VID1 ||
+												 vid == XBOX_VID2 ||
+												 vid == XBOX_VID3 ||
+												 vid == XBOX_VID4 ||
+												 vid == XBOX_VID5 ||
+												 vid == XBOX_VID6 ||
+												 vid == XBOX_VID7) &&
+
+												(pid == XBOX_ONE_PID1 ||
+												 pid == XBOX_ONE_PID2 ||
+												 pid == XBOX_ONE_PID3 ||
+												 pid == XBOX_ONE_PID4 ||
+												 pid == XBOX_ONE_PID5 ||
+												 pid == XBOX_ONE_PID6 ||
+												 pid == XBOX_ONE_PID7 ||
+												 pid == XBOX_ONE_PID8 ||
+												 pid == XBOX_ONE_PID9 ||
+												 pid == XBOX_ONE_PID10 ||
+												 pid == XBOX_ONE_PID11 ||
+												 pid == XBOX_ONE_PID12 ||
+												 pid == XBOX_ONE_PID13 ||
+												 pid == XBOX_ONE_PID14 ||
+												 pid == XBOX_ONE_PID15 ||
+												 pid == XBOX_ONE_PID16));
         };
         /**@}*/
 
