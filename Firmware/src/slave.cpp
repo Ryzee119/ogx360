c2ef6839f2357a644bb0f91f60321e2ad8e03510
Ryzee119 <wendland@live.com.au>
2021-06-10 11:14:25 +0930

    Create slave.cpp

---
+++ Firmware/src/slave.cpp
@@ -0,0 +1,34 @@
+// Copyright 2020, Ryan Wendland, ogx360
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#include "main.h"
+
+extern usbd_controller_t usbd_c[MAX_GAMEPADS];
+
+void i2c_get_data(int len)
+{
+
+}
+
+void i2c_send_data(void)
+{
+
+}
+
+void slave_init(void)
+{
+    uint8_t slave_id = digitalRead(PLAYER_ID1_PIN) << 1 | digitalRead(PLAYER_ID2_PIN);
+    Wire.begin(slave_id);
+    Wire.setClock(400000);
+    Wire.onRequest(i2c_send_data);
+    Wire.onReceive(i2c_get_data);
+    setWireTimeout(4000, true);
+}
+
+void slave_task(void)
+{
+    //nothing to do!
+}
