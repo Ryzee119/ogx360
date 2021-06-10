3e4fde21612e38cd28e4a8abf6ade8c5c3330e67
Ryzee119 <wendland@live.com.au>
2021-06-10 11:48:49 +0930

    Update slave.cpp

--- Firmware/src/slave.cpp
+++ Firmware/src/slave.cpp
@@ -10,7 +10,51 @@ extern usbd_controller_t usbd_c[MAX_GAMEPADS];
 
 void i2c_get_data(int len)
 {
+    uint8_t start = Wire.read();
 
+    //0xAA is a ping to see if the slave module is connected
+    //Flash the LED to confirm receipt.
+    if (start[0] == 0xAA)
+    {
+        digitalWrite(ARDUINO_LED_PIN, LOW);
+        delay(250);
+        digitalWrite(ARDUINO_LED_PIN, HIGH);
+        goto flush;
+    }
+
+    //Controller state packet 0xFx, where 'x' is the controller type.
+    if (start & 0xF0 == 0xF0)
+    {
+        usbd_c.type = start & 0x0F;
+        if (usbd_c.type == DISCONNECTED)
+        {
+            goto flush;
+        }
+
+        uint8_t *rxbuf = NULL;
+        uint8_t rxlen = 0;
+        if (usbd_c.type == DUKE)
+        {
+            if (len != usbd_c.duke.in + 1)
+                goto flush;
+            rxbuf = (uint8_t*)&usbd_c.duke.in;
+            rxlen = sizeof(usbd_c.duke.in);
+        }
+        else if (usbd_c.type == STEELBATTALTION)
+        {
+            if (len != usbd_c.sb.in + 1)
+                goto flush;
+            rxbuf = (uint8_t*)&usbd_c.sb.in;
+            rxlen = sizeof(usbd_c.sb.in);
+        }
+        for (uint8_t i = 0; i < rxlen; i++)
+        {
+            rxbuf[i] = Wire.read();
+        }
+    }
+
+flush:
+    Wire.flush();
 }
 
 void i2c_send_data(void)
