891720b631f476230adb030a194bfc929d27dc2e
Ryan Wendland <wendland@live.com.au>
2021-06-12 12:41:33 +0930

    USBD: Add PluggableUSB XID driver

---
+++ Firmware/src/usbd/usbd_xid.cpp
@@ -0,0 +1,169 @@
+// Copyright 2021, Ryan Wendland, ogx360
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "usbd_xid.h"
+
+//#define ENABLE_USBD_XID_DEBUG
+#ifdef ENABLE_USBD_XID_DEBUG
+#define USBD_XID_DEBUG(a) Serial1.print(F(a))
+#else
+#define USBD_XID_DEBUG(...)
+#endif
+
+XID_ &XID()
+{
+    static XID_ obj;
+    return obj;
+}
+
+int XID_::getInterface(uint8_t *interfaceCount)
+{
+    *interfaceCount += 1;
+
+    XIDDescriptor xid_interface = {
+        D_INTERFACE(pluggedInterface, 2, XID_INTERFACECLASS, XID_INTERFACESUBCLASS, 0),
+        D_ENDPOINT(USB_ENDPOINT_IN(XID_EP_IN), USB_ENDPOINT_TYPE_INTERRUPT, USB_EP_SIZE, 0x04),
+        D_ENDPOINT(USB_ENDPOINT_OUT(XID_EP_OUT), USB_ENDPOINT_TYPE_INTERRUPT, USB_EP_SIZE, 0x04)};
+
+    return USB_SendControl(0, &xid_interface, sizeof(xid_interface));
+}
+
+int XID_::getDescriptor(USBSetup &setup)
+{
+    //Device descriptor for duke, seems to work fine for Steel Battalion. Keep constant.
+    USB_SendControl(TRANSFER_PGM, &xid_dev_descriptor, sizeof(xid_dev_descriptor));
+    return sizeof(xid_dev_descriptor);
+}
+
+int XID_::sendReport(const void *data, int len)
+{
+    int capped_len = min((unsigned int)len, sizeof(xid_in_data));
+    if (memcmp(xid_in_data, data, capped_len) != 0)
+    {
+        //Update local copy, then send
+        if (USB_Send(XID_EP_IN | TRANSFER_RELEASE, data, capped_len) == len)
+            memcpy(xid_in_data, data, capped_len);
+    }
+    return len;
+}
+
+int XID_::getReport(void *data, int len)
+{
+    int capped_len = min((uint32_t)len, sizeof(xid_out_data));
+    uint8_t r[capped_len] = {0};
+    if (USB_Recv(XID_EP_OUT | TRANSFER_RELEASE, r, capped_len) == capped_len)
+    {
+        USBD_XID_DEBUG("USBD XID: GOT HID REPORT OUT FROM ENDPOINT\n");
+        memcpy(xid_out_data, r, capped_len);
+        memcpy(data, r, capped_len);
+        return capped_len;
+    }
+    //No new data on interrupt pipe, return previous data
+    memcpy(data, xid_out_data, capped_len);
+    return xid_out_data[1];
+}
+
+bool XID_::setup(USBSetup &setup)
+{
+    if (pluggedInterface != setup.wIndex)
+    {
+        return false;
+    }
+
+    uint8_t request = setup.bRequest;
+    uint8_t requestType = setup.bmRequestType;
+    uint16_t wValue = (setup.wValueH << 8) | (setup.wValueL & 0xFF);
+
+    if (requestType == (REQUEST_DEVICETOHOST | REQUEST_VENDOR | REQUEST_INTERFACE))
+    {
+        if (request == 0x06 && wValue == 0x4200)
+        {
+            USBD_XID_DEBUG("USBD XID: SENDING XID DESCRIPTOR\n");
+            if (xid_type == DUKE)
+            {
+                USB_SendControl(TRANSFER_PGM, DUKE_DESC_XID, sizeof(DUKE_DESC_XID));
+            }
+            else if (xid_type == STEELBATTALTION)
+            {
+                USB_SendControl(TRANSFER_PGM, BATTALION_DESC_XID, sizeof(BATTALION_DESC_XID));
+            }
+            return true;
+        }
+        if (request == 0x01 && wValue == 0x0100)
+        {
+            USBD_XID_DEBUG("USBD XID: SENDING XID CAPABILITIES IN\n");
+            USB_SendControl(TRANSFER_PGM, DUKE_CAPABILITIES_IN, sizeof(DUKE_CAPABILITIES_IN));
+            return true;
+        }
+        if (request == 0x01 && wValue == 0x0200)
+        {
+            USBD_XID_DEBUG("USBD XID: SENDING XID CAPABILITIES OUT\n");
+            USB_SendControl(TRANSFER_PGM, DUKE_CAPABILITIES_OUT, sizeof(DUKE_CAPABILITIES_OUT));
+            return true;
+        }
+    }
+
+    if (requestType == (REQUEST_DEVICETOHOST | REQUEST_CLASS | REQUEST_INTERFACE))
+    {
+        if (request == HID_GET_REPORT && setup.wValueH == HID_REPORT_TYPE_INPUT)
+        {
+            USBD_XID_DEBUG("USBD XID: SENDING HID REPORT IN\n");
+            USB_SendControl(0, xid_in_data, sizeof(xid_in_data));
+            return true;
+        }
+    }
+
+    if (requestType == (REQUEST_HOSTTODEVICE | REQUEST_CLASS | REQUEST_INTERFACE))
+    {
+        if (request == HID_SET_REPORT && setup.wValueH == HID_REPORT_TYPE_OUTPUT)
+        {
+            USBD_XID_DEBUG("USBD XID: GETTING HID REPORT OUT\n");
+            uint16_t length = min(sizeof(xid_out_data), setup.wLength);
+            USB_RecvControl(xid_out_data, length);
+            return true;
+        }
+    }
+
+    USBD_XID_DEBUG("USBD XID: STALL\n");
+    Serial1.println(requestType, HEX);
+    Serial1.println(request, HEX);
+    Serial1.println(wValue, HEX);
+    return false;
+}
+
+void XID_::setType(xid_type_t type)
+{
+    if (xid_type == type)
+    {
+        return;
+    }
+
+    xid_type = type;
+    UDCON |= (1 << DETACH);
+    delay(10);
+    if (xid_type != DISCONNECTED)
+    {
+        UDCON &= ~(1 << DETACH);
+    } 
+    return;
+}
+
+xid_type_t XID_::getType(void)
+{
+    return xid_type;
+}
+
+XID_::XID_(void) : PluggableUSBModule(2, 1, epType)
+{
+    epType[0] = EP_TYPE_INTERRUPT_IN;
+    epType[1] = EP_TYPE_INTERRUPT_OUT;
+    memset(xid_out_data, 0x00, sizeof(xid_out_data));
+    memset(xid_in_data, 0x00, sizeof(xid_in_data));
+    xid_type = DUKE;
+    PluggableUSB().plug(this);
+}
+
+int XID_::begin(void)
+{
+    return 0;
+}
---
+++ Firmware/src/usbd/usbd_xid.h
@@ -0,0 +1,306 @@
+// Copyright 2021, Ryan Wendland, ogx360
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef _XID_h
+#define _XID_h
+
+#define _USING_XID
+
+#include <Arduino.h>
+#include <PluggableUSB.h>
+
+#define XID_INTERFACECLASS 88
+#define XID_INTERFACESUBCLASS 66
+
+#ifndef _USING_HID
+#define HID_GET_REPORT 0x01
+#define HID_SET_REPORT 0x09
+#define HID_REPORT_TYPE_INPUT 1
+#define HID_REPORT_TYPE_OUTPUT 2
+#endif
+
+#define XID_EP_IN (pluggedEndpoint)
+#define XID_EP_OUT (pluggedEndpoint + 1)
+
+#define DUKE_DUP (1 << 0)
+#define DUKE_DDOWN (1 << 1)
+#define DUKE_DLEFT (1 << 2)
+#define DUKE_DRIGHT (1 << 3)
+#define DUKE_START (1 << 4)
+#define DUKE_BACK (1 << 5)
+#define DUKE_LS (1 << 6)
+#define DUKE_RS (1 << 7)
+
+//https://github.com/Cxbx-Reloaded/Cxbx-Reloaded/blob/develop/src/core/hle/XAPI/Xapi.h
+#define SBC_W0_RIGHTJOYMAINWEAPON 0x0001
+#define SBC_W0_RIGHTJOYFIRE 0x0002
+#define SBC_W0_RIGHTJOYLOCKON 0x0004
+#define SBC_W0_EJECT 0x0008
+#define SBC_W0_COCKPITHATCH 0x0010
+#define SBC_W0_IGNITION 0x0020
+#define SBC_W0_START 0x0040
+#define SBC_W0_MULTIMONOPENCLOSE 0x0080
+#define SBC_W0_MULTIMONMAPZOOMINOUT 0x0100
+#define SBC_W0_MULTIMONMODESELECT 0x0200
+#define SBC_W0_MULTIMONSUBMONITOR 0x0400
+#define SBC_W0_MAINMONZOOMIN 0x0800
+#define SBC_W0_MAINMONZOOMOUT 0x1000
+#define SBC_W0_FUNCTIONFSS 0x2000
+#define SBC_W0_FUNCTIONMANIPULATOR 0x4000
+#define SBC_W0_FUNCTIONLINECOLORCHANGE 0x8000
+
+#define SBC_W1_WASHING 0x0001
+#define SBC_W1_EXTINGUISHER 0x0002
+#define SBC_W1_CHAFF 0x0004
+#define SBC_W1_FUNCTIONTANKDETACH 0x0008
+#define SBC_W1_FUNCTIONOVERRIDE 0x0010
+#define SBC_W1_FUNCTIONNIGHTSCOPE 0x0020
+#define SBC_W1_FUNCTIONF1 0x0040
+#define SBC_W1_FUNCTIONF2 0x0080
+#define SBC_W1_FUNCTIONF3 0x0100
+#define SBC_W1_WEAPONCONMAIN 0x0200
+#define SBC_W1_WEAPONCONSUB 0x0400
+#define SBC_W1_WEAPONCONMAGAZINE 0x0800
+#define SBC_W1_COMM1 0x1000
+#define SBC_W1_COMM2 0x2000
+#define SBC_W1_COMM3 0x4000
+#define SBC_W1_COMM4 0x8000
+
+#define SBC_W2_COMM5 0x0001
+#define SBC_W2_LEFTJOYSIGHTCHANGE 0x0002
+#define SBC_W2_TOGGLEFILTERCONTROL 0x0004
+#define SBC_W2_TOGGLEOXYGENSUPPLY 0x0008
+#define SBC_W2_TOGGLEFUELFLOWRATE 0x0010
+#define SBC_W2_TOGGLEBUFFREMATERIAL 0x0020
+#define SBC_W2_TOGGLEVTLOCATION 0x0040
+
+#define SBC_AXIS_AIMINGX 0x0001
+#define SBC_AXIS_AIMINGY 0x0002
+#define SBC_AXIS_LEVER   0x0004
+#define SBC_AXIS_SIGHTX  0x0008
+#define SBC_AXIS_SIGHTY  0x0010
+#define SBC_AXIS_LPEDAL  0x0020
+#define SBC_AXIS_MPEDAL  0x0040
+#define SBC_AXIS_RPEDAL  0x0080
+#define SBC_AXIS_TUNER   0x0100
+#define SBC_AXIS_GEAR    0x0200
+
+
+static const DeviceDescriptor xid_dev_descriptor PROGMEM =
+    D_DEVICE(0x00, 0x00, 0x00, USB_EP_SIZE, USB_VID, USB_PID, 0x0121, 0, 0, 0, 1);
+
+static const uint8_t DUKE_DESC_XID[] PROGMEM = {
+    0x10,
+    0x42,
+    0x00, 0x01,
+    0x01,
+    0x02,
+    0x14,
+    0x06,
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+static const uint8_t DUKE_CAPABILITIES_IN[] PROGMEM = {
+    0x00,
+    0x14,
+    0xFF,
+    0x00,
+    0xFF,
+    0xFF, 0xFF, 0xFF,
+    0xFF, 0xFF, 0xFF,
+    0xFF, 0xFF, 0xFF,
+    0xFF, 0xFF, 0xFF,
+    0xFF, 0xFF, 0xFF};
+
+static const uint8_t DUKE_CAPABILITIES_OUT[] PROGMEM = {
+    0x00,
+    0x06,
+    0xFF, 0xFF, 0xFF, 0xFF};
+
+static const uint8_t BATTALION_DESC_XID[] PROGMEM = {
+    0x10,
+    0x42,
+    0x00, 0x01,
+    0x80,
+    0x01,
+    0x1A,
+    0x16,
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+typedef struct __attribute__((packed))
+{
+    InterfaceDescriptor interface;
+    EndpointDescriptor in;
+    EndpointDescriptor out;
+} XIDDescriptor;
+
+typedef struct __attribute__((packed))
+{
+    uint8_t startByte;
+    uint8_t bLength;
+    uint16_t wButtons;
+    uint8_t A;
+    uint8_t B;
+    uint8_t X;
+    uint8_t Y;
+    uint8_t BLACK;
+    uint8_t WHITE;
+    uint8_t L;
+    uint8_t R;
+    int16_t leftStickX;
+    int16_t leftStickY;
+    int16_t rightStickX;
+    int16_t rightStickY;
+} usbd_duke_in_t;
+
+typedef struct __attribute__((packed))
+{
+    uint8_t startByte;
+    uint8_t bLength;
+    uint16_t lValue;
+    uint16_t hValue;
+} usbd_duke_out_t;
+
+typedef struct __attribute__((packed))
+{
+    usbd_duke_in_t in;
+    usbd_duke_out_t out;
+} usbd_duke_t;
+
+typedef struct __attribute__((packed))
+{
+    uint8_t startByte;
+    uint8_t bLength;
+    uint16_t wButtons[3];
+    int16_t aimingX;
+    int16_t aimingY;
+    int16_t rotationLever;
+    int16_t sightChangeX;
+    int16_t sightChangeY;
+    uint16_t leftPedal;
+    uint16_t middlePedal;
+    uint16_t rightPedal;
+    int8_t tunerDial;
+    int8_t gearLever;
+} usbd_sbattalion_in_t;
+
+typedef struct __attribute__((packed))
+{
+    uint8_t startByte;
+    uint8_t bLength;
+    union {
+        uint8_t CockpitHatch : 4;
+        uint8_t EmergencyEject : 4;
+    };
+    union {
+    uint8_t Start : 4;
+    uint8_t Ignition : 4;
+    };
+    union {
+    uint8_t MapZoomInOut: 4;
+    uint8_t OpenClose: 4;
+    };
+    union {
+    uint8_t SubMonitorModeSelect: 4;
+    uint8_t ModeSelect: 4;
+    };
+    union {
+    uint8_t MainMonitorZoomOut: 4;
+    uint8_t MainMonitorZoomIn: 4;
+    };
+    union {
+    uint8_t Manipulator: 4;
+    uint8_t ForecastShootingSystem: 4;
+    };
+    union {
+    uint8_t Washing: 4;
+    uint8_t LineColorChange: 4;
+    };
+    union {
+    uint8_t Chaff: 4;
+    uint8_t Extinguisher: 4;
+    };
+    union {
+    uint8_t Override: 4;
+    uint8_t TankDetach: 4;
+    };
+    union {
+    uint8_t F1: 4;
+    uint8_t NightScope: 4;
+    };
+    union {
+    uint8_t F3: 4;
+    uint8_t F2: 4;
+    };
+    union {
+    uint8_t SubWeaponControl: 4;
+    uint8_t MainWeaponControl: 4;
+    };
+    union {
+    uint8_t Comm1: 4;
+    uint8_t MagazineChange: 4;
+    };
+    union {
+    uint8_t Comm3: 4;
+    uint8_t Comm2: 4;
+    };
+    union {
+    uint8_t Comm5: 4;
+    uint8_t Comm4: 4;
+    };
+    union {
+    uint8_t GearR: 4;
+    uint8_t unused1: 4;
+    };
+    union {
+    uint8_t Gear1: 4;
+    uint8_t GearN: 4;
+    };
+    union {
+    uint8_t Gear3: 4;
+    uint8_t Gear2: 4;
+    };
+    union {
+    uint8_t Gear5: 4;
+    uint8_t Gear4: 4;
+    };
+    uint8_t unused2;
+} usbd_sbattalion_out_t;
+
+typedef struct __attribute__((packed))
+{
+    usbd_sbattalion_in_t in;
+    usbd_sbattalion_out_t out;
+} usbd_steelbattalion_t;
+
+typedef enum
+{
+    DISCONNECTED = 0,
+    DUKE,
+    STEELBATTALTION
+} xid_type_t;
+
+class XID_ : public PluggableUSBModule
+{
+public:
+    XID_(void);
+    int begin(void);
+    int sendReport(const void *data, int len);
+    int getReport(void *data, int len);
+    void setType(xid_type_t type);
+    xid_type_t getType(void);
+
+protected:
+    int getInterface(uint8_t *interfaceCount);
+    int getDescriptor(USBSetup &setup);
+    bool setup(USBSetup &setup);
+
+private:
+    xid_type_t xid_type;
+    uint8_t epType[2];
+    uint8_t xid_in_data[32];
+    uint8_t xid_out_data[32];
+};
+
+XID_ &XID();
+
+#endif // _XID_h
